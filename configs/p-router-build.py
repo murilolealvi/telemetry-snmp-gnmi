a94db5ca13b53ee65bc25308c8a739f031483db0
Murilo Gabriel <muriloleal2008@hotmail.com>
2026-06-12 09:52:48 -0300

    enabling LDP on P-routers

--- configs/p-router-build.py
+++ configs/p-router-build.py
@@ -8,21 +8,21 @@ with open('p-router.j2', 'r') as file:
 routers = [
     {
         "hostname": "p1",
-        "loopback_ip": "10.0.0.13/32",
+        "loopback_ip": "10.0.0.30/32",
         "eth1_ip": "10.1.1.1/31",    # to PE1
         "eth2_ip": "10.1.12.0/31",   # to P2
         "eth3_ip": "10.1.13.1/31"    # to P3
     },
     {
         "hostname": "p2",
-        "loopback_ip": "10.0.0.14/32",
+        "loopback_ip": "10.0.0.40/32",
         "eth1_ip": "10.1.2.1/31",    # to PE2
         "eth2_ip": "10.1.12.1/31",   # to P1
         "eth3_ip": "10.1.23.1/31"    # to P3
     },
     {
         "hostname": "p3",
-        "loopback_ip": "10.0.0.15/32",
+        "loopback_ip": "10.0.0.50/32",
         "eth1_ip": "10.1.23.0/31",   # to P2
         "eth2_ip": "10.1.13.0/31",   # to P1
         "eth3_ip": "127.0.0.1/8"     # dummy config
